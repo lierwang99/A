174695606cbfa8b82c2a13c3d034ef40d71f16c8
lierwang <1219838266@qq.com>
2020-02-02 22:51:20 +0800

    V1.1 : 增加b包，B.java
    Signed-off-by: lierwang <1219838266@qq.com>

---
+++ src/b/B.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package b;
+
+/**
+ * @author Administrator
+ *
+ */
+public class B {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(321);
+	}
+
+}
