048add254dbcd6410ccbcbced65a8c200db99175
lierwang <1219838266@qq.com>
2020-02-02 22:47:26 +0800

    V1.0 : A项目首次上传
    Signed-off-by: lierwang <1219838266@qq.com>

---
+++ src/a/A.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package a;
+
+/**
+ * @author Administrator
+ *
+ */
+public class A {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub、
+		System.out.println(123);
+
+	}
+
+}
